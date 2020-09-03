ff97bd6f99bdcc42ba41f7de38cd639f4a39cc55
Darkowl999 <raulalbertoortiz290199@hotmail.com>
2020-09-03 14:03:18 -0400

    sintaxis basica casi finalizada

--- Basic_sintax/polimorfismo.py
+++ Basic_sintax/polimorfismo.py
@@ -14,5 +14,8 @@ class Camion():
         print("me desplazo utilizando seis ruedas")
 
 
-def desplazamientoVehiculo(vehiculo):#
-    vehiculo.desplazamiento()
+def desplazamientoVehiculo(vehiculo):#aca se aplica el polimorfismo
+    vehiculo.desplazamiento() 
+
+miVehiculo=Camion()
+desplazamientoVehiculo(miVehiculo)
